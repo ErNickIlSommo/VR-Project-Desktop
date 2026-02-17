09f938aaae94017892ad7eb8bbc90a97a0056e4b
ErNickIlSommo <nick.gianuzzi99@gmail.com>
2026-02-18 00:56:14 +0100

    Add nurse activity

---
+++ Assets/Scripts/Activities/Nurse/NurseActivity.cs
@@ -0,0 +1,125 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class NurseActivity : MonoBehaviour
+{
+    public event Action<bool> OnActivityStarted;
+    public event Action<bool> OnActivityCompleted;
+    
+    [SerializeField] private bool _isActivityStarted = false;
+    [SerializeField] private bool _isActivityCompleted = false;
+    [SerializeField] private int _score = 0;
+    [SerializeField] private int _failedRequests = 0;
+    
+    private float[] _cooldowns = { 20f, 15f, 13f };
+    private const int _lvl1 = 6;
+    private const int _lvl2 = 9;
+    private const int _lvl3 = 11;
+    
+    [SerializeField] private float _currentCooldown;
+
+    [SerializeField] private Larvas larvasManager;
+    [SerializeField] private List<GrabbableObjectData> larvaRequestOptions;
+
+    private bool _isForced;
+    
+    // Getters and Setters
+    
+    
+    public bool IsActivityStarted 
+    {
+        get { return _isActivityStarted; }
+        set { _isActivityStarted = value; }
+    }
+    
+    public bool IsActivityCompleted => _isActivityCompleted;
+    
+    public int Score => _score;
+
+    
+    // Methods
+
+    private void Awake()
+    {
+        larvasManager.SendInfoToMaster += HandleLarvaTerminated;
+        larvasManager.IsLarvasTerminated += HandleAllLarvasTerminated;
+    }
+
+    public void StartActivity(int howMany = 5000)
+    {
+        if (_isActivityStarted) return;
+        
+        if (howMany <= 0) howMany = 5000;
+        
+        
+        _score = 0;
+        _failedRequests = 0;
+        _currentCooldown = _cooldowns[0];
+        larvasManager.InitLarvasManager();
+        _isActivityCompleted = false;
+        _isActivityStarted = true;
+        _isForced = false;
+        
+        if(OnActivityStarted != null) OnActivityStarted.Invoke(_isActivityCompleted);
+        
+        // Generate a request with a random Ingredient
+        int ingredientIndex = Random.Range(0, larvaRequestOptions.Count);
+        larvasManager
+            .SendRequestToLarvas(
+                larvaRequestOptions[ingredientIndex],
+                _currentCooldown
+            );
+    }
+
+    private void HandleLarvaTerminated(LarvaEventInfo eventInfo)
+    {
+        if (!_isActivityStarted) return;
+        if (eventInfo == null) return;
+        
+        // If the Player is dumb
+        if (!eventInfo.RequestResutlt)
+        {
+            _failedRequests++;
+            if (_failedRequests >= 3)
+            {
+                StopActivity();
+                return;
+            }
+        } else _score++;
+        
+        // Generate another request
+        var level = larvasManager.Index + 1;
+        if(level >= _lvl1) _currentCooldown = _cooldowns[1];
+        else if (level >= _lvl2) _currentCooldown = _cooldowns[2];
+        else if (level >= _lvl3) _currentCooldown = _cooldowns[3];
+        else _currentCooldown = _cooldowns[0];
+        
+        int ingredientIndex = Random.Range(0, larvaRequestOptions.Count);
+        larvasManager
+            .SendRequestToLarvas(
+                larvaRequestOptions[ingredientIndex],
+                _currentCooldown
+            );
+    }
+
+    private void HandleAllLarvasTerminated(bool status)
+    {
+        if (!status) return;
+        StopActivity();
+    }
+    
+    public void StopActivity()
+    {
+        if (!_isActivityStarted) return;
+        
+        if (_score >= larvasManager.NurseLarvas.Count - 3)
+            _isActivityCompleted = true;
+        else _isActivityCompleted = false;
+        
+        _isActivityStarted = false;
+        
+        if (OnActivityCompleted != null) OnActivityCompleted.Invoke(_isActivityCompleted);
+    }
+}
--- Assets/Scripts/Interactions/LarvaInteraction.cs
+++ Assets/Scripts/Interactions/LarvaInteraction.cs
@@ -1,31 +1,67 @@
+using System;
 using UnityEngine;
 using System.Collections;
 
 public class LarvaInteraction : MonoBehaviour, IInteractable
 {
+    // Event for signaling to master
+    public event Action<LarvaEventInfo> OnRequestTerminated;
+    
+    // Identifier (assigned by master)
+    private int _larvaId;
+    
+    // Request and received Objects
     private GrabbableObjectData _requestedObject;
+    private GrabbableObjectData _receivedObject;
+    
+    // Time variables
+    [SerializeField] private float _cooldown = 10f;
+    [SerializeField] private float _timer = 0f;
+    private Coroutine _timerRunningCoroutine; 
+    
+    // Flag controlling the request
+    [SerializeField] private bool _isRequestRunning = false; 
+    [SerializeField] private bool _isRequestCorrect;
+
+    /*
+     * Test functionalities
+     */
+    [SerializeField] private Renderer renderer;
+    private Color _defaultColor = Color.darkGray;
+    private Color _requestColor = Color.blue;
+    private Color _correctColor = Color.green;
+    private Color _incorrectColor = Color.red;
+    
+
+    // Getters and Setters
+    
+    
+    public int LarvaId { get => _larvaId; set => _larvaId = value; }
+    
     public GrabbableObjectData RequestedObject
     {
         get => _requestedObject;
         set => _requestedObject = value;
     } 
     
-    private GrabbableObjectData _receivedObject;
-    
-    [SerializeField] private float _cooldown = 10f;
     public float Cooldown
     {
         get => _cooldown;
         set => _cooldown = value;
-    } 
+    }  
     
-    [SerializeField] private bool _isRequestRunning = false; 
-    public bool IsRequestRunning { get => _isRequestRunning; } 
+    public bool IsRequestRunning => _isRequestRunning;
+    public bool IsRequestCorrect => _isRequestCorrect;
     
-    private Coroutine _timerRunningCoroutine;
-    [SerializeField] private bool _isRequestCorrect;
+    // Methods
 
-    [SerializeField] private float _timer = 0f;
+    /*
+     * Test Functionalities
+     */
+    public void InitLarva()
+    {
+        renderer.material.color = _defaultColor;
+    }
 
     public bool StartRequest(GrabbableObjectData requestedObject, float cooldown = float.NaN)
     {
@@ -40,7 +76,11 @@ public class LarvaInteraction : MonoBehaviour, IInteractable
         _requestedObject = requestedObject;
         _isRequestRunning = true; 
         
-        Debug.Log("Started request item with id: " + _requestedObject.Id);
+        /*
+         * Test functionalities
+         */
+        Debug.Log("Started request item: " + _requestedObject.Name);
+        renderer.material.color = _requestColor;
         
         return true;
     }
@@ -55,6 +95,7 @@ public class LarvaInteraction : MonoBehaviour, IInteractable
         
         if (!IsCorrectIngredient(interactor))
         {
+            _isRequestCorrect = false;
             RefuseIngredient();
             interactor.PlayerInteractionStatus.GrabbableObject.ForceDropAndDestroy(interactor);
             return false;
@@ -68,6 +109,9 @@ public class LarvaInteraction : MonoBehaviour, IInteractable
 
     private bool IsCorrectIngredient(Interactor interactor)
     {
+        if(!interactor.PlayerInteractionStatus.HasGrabbed) return false;
+        if (!interactor.PlayerInteractionStatus.ObjectData) return false;
+        
         int objectDataId = interactor.PlayerInteractionStatus.ObjectData.Id;
         
         return _requestedObject.Id == objectDataId;
@@ -83,8 +127,20 @@ public class LarvaInteraction : MonoBehaviour, IInteractable
         
         _requestedObject = null;
         _isRequestRunning = false;
-        
+
+        /*
+         * Test functionalities
+         */
         Debug.Log("Task failed");
+        renderer.material.color = _incorrectColor;
+        
+        if (OnRequestTerminated != null) 
+            OnRequestTerminated.Invoke(
+                new LarvaEventInfo(
+                    _larvaId,
+                    transform.gameObject.name,
+                    false
+            ));
     }
 
     private void EatIngredient()
@@ -93,7 +149,19 @@ public class LarvaInteraction : MonoBehaviour, IInteractable
         _timerRunningCoroutine = null;
         _isRequestRunning = false;
         
+        /*
+         * Test functionalities
+         */
         Debug.Log("Task succeded");
+        renderer.material.color = _correctColor;
+        
+        if (OnRequestTerminated != null) 
+            OnRequestTerminated.Invoke(
+                new LarvaEventInfo(
+                    _larvaId,
+                    transform.gameObject.name,
+                    true
+            ));
     }
     
     private IEnumerator Timer()
