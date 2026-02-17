fc9615d023ffdf173804ea36185ee520c347a074
Mike <casadidiomichele2001@gmail.com>
2026-02-17 09:59:46 +0100

    Ripristinato volo ed inserite animazioni, refactoring ancora in corso

--- Assets/Scripts/Movement/ThirdPersonController.cs
+++ Assets/Scripts/Movement/ThirdPersonController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,6 +10,7 @@ public class ThirdPersonController : MonoBehaviour
     [SerializeField] private string actionMapName = "Controls";
     [SerializeField] private string moveActionName = "Move";
     [SerializeField] private string lookActionName = "Look";
+    [SerializeField] private string flyActionName = "Fly";
 
     //[Header("Status")]
     //[SerializeField] private PlayerStatus status;
@@ -17,12 +19,13 @@ public class ThirdPersonController : MonoBehaviour
     [SerializeField] private float walkSpeed = 5f;
     [SerializeField] private float turnSpeed = 12f;
 
-    //[Header("Flight")]
-    //[SerializeField] private float flySpeed = 15f;
-    //[SerializeField] private bool isGrounded = true;
-    //[SerializeField] private float maxIncline = 15f;
-    //[SerializeField] private float minIncline = -15f;
-    //[SerializeField] private float heightChangeSpeed = 12f;
+    [Header("Flight")]
+    [SerializeField] private float flySpeed = 15f;
+    [SerializeField] private bool isFlying = false;
+    [SerializeField] private float maxIncline = 5f;
+    [SerializeField] private float minIncline = -10f;
+    [SerializeField] private float heightChangeSpeed = 12f;
+    [SerializeField] private float gravity = -10f;
 
     [Header("Look")]
     [SerializeField] private float rotateSpeed = 5f;
@@ -35,7 +38,7 @@ public class ThirdPersonController : MonoBehaviour
     [SerializeField] private Animator animator;
     //[SerializeField] private string speedParam = "Speed";
     [SerializeField] private string movingParam = "IsWalking";
-    //[SerializeField] private string flyingParam = "IsFlying";
+    [SerializeField] private string flyingParam = "IsFlying";
     [SerializeField] private float animSmooth = 10f;
 
     private float m_animSpeed;
@@ -46,9 +49,11 @@ public class ThirdPersonController : MonoBehaviour
     private CharacterController m_controller;
     private InputAction m_moveAction;
     private InputAction m_lookAction;
+    private InputAction m_flyAction;
 
     private Vector2 m_moveValue;
     private Vector2 m_lookValue;
+    private float m_flyValue;
 
     private void OnEnable()
     {
@@ -66,7 +71,7 @@ public class ThirdPersonController : MonoBehaviour
         InputActionMap map = inputActions.FindActionMap(actionMapName, true);
         m_moveAction = map.FindAction(moveActionName, true);
         m_lookAction = map.FindAction(lookActionName, true);
-
+        m_flyAction = map.FindAction(flyActionName, true);
 
         m_controller = GetComponent<CharacterController>();
     }
@@ -75,9 +80,14 @@ public class ThirdPersonController : MonoBehaviour
     {
         m_moveValue = m_moveAction.ReadValue<Vector2>();
         m_lookValue = m_lookAction.ReadValue<Vector2>();
+        m_flyValue = m_flyAction.ReadValue<float>();
+        Debug.Log(m_flyValue);
 
-        Move();
         RotateCamera();
+        Move();
+        Fly();
+        ApplyGravity();
+        UpdateFlight();
         UpdateAnimations();
 
     }
@@ -105,6 +115,41 @@ public class ThirdPersonController : MonoBehaviour
         m_controller.Move(dir * walkSpeed * Time.deltaTime);
     }
 
+    private void Fly() 
+    {
+        Vector3 up = transform.up;
+
+        if (m_flyValue == 0.0f) return;
+
+        Debug.Log(m_flyValue);
+        Debug.Log(m_controller.isGrounded);
+        Debug.Log(isFlying);
+
+        if (!isFlying && m_flyValue >= 0.0f)
+        {
+            isFlying = true;
+            UpdateAnimations();
+        }
+
+        m_controller.Move(up * m_flyValue * heightChangeSpeed * Time.deltaTime);
+
+    }
+
+    private void ApplyGravity()
+    {
+        Vector3 up = transform.up;
+        if (isFlying || m_controller.isGrounded) return;
+
+        m_controller.Move(up * gravity * Time.deltaTime);
+    }
+
+    private void UpdateFlight()
+    {
+        if (m_controller.isGrounded)
+            isFlying = false;
+        
+    }
+
     private void RotateCamera()
     {
         if (cameraTarget == null) return;
@@ -121,13 +166,19 @@ public class ThirdPersonController : MonoBehaviour
     {
         if (animator == null) return;
 
-        float target = Mathf.Clamp01(m_moveValue.magnitude);
+        if (!isFlying)
+        {
+            float target = Mathf.Clamp01(m_moveValue.magnitude);
+            m_animSpeed = Mathf.Lerp(m_animSpeed, target, animSmooth * Time.deltaTime);
 
-        m_animSpeed = Mathf.Lerp(m_animSpeed, target, animSmooth * Time.deltaTime);
-
-        //animator.SetFloat(speedParam, m_animSpeed);
-
-        if (!string.IsNullOrEmpty(movingParam))
+            //animator.SetFloat(speedParam, m_animSpeed);
+            animator.SetBool(flyingParam, false);
             animator.SetBool(movingParam, m_animSpeed > 0.05f);
+        }
+
+        if (isFlying)
+        {
+            animator.SetBool(flyingParam, true);
+        }
     }
 }
