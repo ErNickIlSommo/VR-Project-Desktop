c66761511e36b926f87f6eb5c9e4e06866b85ce3
ErNickIlSommo <nick.gianuzzi99@gmail.com>
2026-02-19 02:34:19 +0100

    things...

--- Assets/Scripts/Global/Exit.cs
+++ Assets/Scripts/Global/Exit.cs
@@ -1,12 +1,43 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class Exit : MonoBehaviour
 {
+
+    [SerializeField] private string sceneName;
+    // [SerializeField] private Fader fader;
+    [SerializeField] private Animator transition;
+    [SerializeField] private float transitionTime = 2f;
+
+    /*private void Start()
+    {
+        fader.FadeIn();
+    }*/
+
+    private void Start()
+    {
+        
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return; 
         
-        Debug.Log("Player Used Entered in exit");
+        //transition.SetTrigger("Guard");
+        StartCoroutine(LoadLevel());
+    }
+    
+
+    private IEnumerator LoadLevel()
+    {
+        // yield return fader.FadeOut();
+        transition.SetTrigger("Start");
+        
+        yield return new WaitForSeconds(transitionTime);
+        
+        // yield return fader.FadeOut();
+        SceneManager.LoadScene(sceneName);
     }
 }
