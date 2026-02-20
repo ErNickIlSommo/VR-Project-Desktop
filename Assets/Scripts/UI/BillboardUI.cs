67e3e63fc0bbfb5ec8e021fd32230596d22c2905
ErNickIlSommo <nick.gianuzzi99@gmail.com>
2026-02-20 18:43:51 +0100

    Removed old scripts

--- Assets/Scripts/UI/BillboardUI.cs
+++ Assets/Scripts/UI/BillboardUI.cs
@@ -7,114 +7,16 @@ public class BillboardUI : MonoBehaviour
 {
     private Camera targetCamera;
 
-    [SerializeField] private Image cloud;
-    [SerializeField] private Image request;
-    [SerializeField] private Image ok;
-    [SerializeField] private Image nope;
-    [SerializeField] private Image beebread;
-    [SerializeField] private Image royaljelly;
-    [SerializeField] private Image water;
-
-    
     void Awake()
     {
         if (targetCamera == null)
             targetCamera = Camera.main;
         
-        cloud.enabled = false;
-        request.enabled = false;
-        ok.enabled = false;
-        nope.enabled = false;
-        beebread.enabled = false;
-        royaljelly.enabled = false;
-        water.enabled = false;
-    }
-
-    public void Restart()
-    {
-        if (!cloud || !request || !ok || !nope || !beebread || !royaljelly)
-        {
-            Debug.Log("Error restart ui: ");
-            return;
-        }
-          /*cloud.enabled = false;
-          request.enabled = false;
-          ok.enabled = false;
-          nope.enabled = false;
-          beebread.enabled = false;
-          royaljelly.enabled = false;
-          water.enabled = false;*/
-    }
-
-    public void OnRequest()
-    {
-        cloud.enabled = true;
-        request.enabled = true;
-        ok.enabled = false;
-        nope.enabled = false;
-        beebread.enabled = false;
-        royaljelly.enabled = false;
-        water.enabled = false;
-    }
-
-    public void OnNope()
-    {
-        cloud.enabled = true;
-        request.enabled = false;
-        ok.enabled = false;
-        nope.enabled = true;
-        beebread.enabled = false;
-        royaljelly.enabled = false;
-        water.enabled = false;
-    }
-
-    public void Ok()
-    {
-        cloud.enabled = true;
-        request.enabled = false;
-        ok.enabled = true;
-        nope.enabled = false;
-        beebread.enabled = false;
-        royaljelly.enabled = false;
-        water.enabled = false;
-    }
-
-    public void Beebread()
-    {
-        cloud.enabled = true;
-        request.enabled = false;
-        ok.enabled = false;
-        nope.enabled = false;
-        beebread.enabled = true;
-        royaljelly.enabled = false;
-        water.enabled = false;
-    }
-
-    public void Royaljelly()
-    {
-        cloud.enabled = true;
-        request.enabled = false;
-        ok.enabled = false;
-        nope.enabled = false;
-        beebread.enabled = false;
-        royaljelly.enabled = true;
-        water.enabled = false;
-    }
-
-    public void Water()
-    {
-        cloud.enabled = true;
-        request.enabled = false;
-        ok.enabled = false;
-        nope.enabled = false;
-        beebread.enabled = false;
-        royaljelly.enabled = false;
-        water.enabled = true;
     }
 
     void LateUpdate()
     {
-        if (targetCamera == null) return;
+        if (!targetCamera) return;
 
         Vector3 dir = transform.position - targetCamera.transform.position;
 
