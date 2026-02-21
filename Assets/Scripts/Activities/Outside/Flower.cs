1b9c89bea585034aec8ff83996d096fa8a5c0aaa
ErNickIlSommo <nick.gianuzzi99@gmail.com>
2026-02-21 04:02:06 +0100

    Fix and add flower UI

--- Assets/Scripts/Activities/Outside/Flower.cs
+++ Assets/Scripts/Activities/Outside/Flower.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class Flower : MonoBehaviour, IInteractable
 {
     public Action<bool, Flower> OnInteraction;
-    
+
+    [SerializeField] private float timeUIToDisappear = 2f;
     [SerializeField] AudioSource m_AudioSource;
+    private FlowerUI _ui;
 
     private FlowerState flowerState;
     
@@ -14,12 +18,14 @@ public class Flower : MonoBehaviour, IInteractable
 
     private void Awake()
     {
-        
+        _ui = GetComponent<FlowerUI>();
     }
 
     public void EnableInteraction()
     {
         _canInteract = true;
+        _ui.UpdateImage(FlowerState.ToForage);
+        _ui.Show();
     }
 
     public void DisableInteraction()
@@ -31,8 +37,16 @@ public class Flower : MonoBehaviour, IInteractable
     {
         Debug.Log("FLOWER " + gameObject.name + " Interaction");
         if (!_canInteract) return false;
+        StartCoroutine(WaitAndUpdateUI());
         if (OnInteraction != null) OnInteraction.Invoke(true, this);
         return true;
+    }
+
+    private IEnumerator WaitAndUpdateUI()
+    {
+        _ui.UpdateImage(FlowerState.Foraged);
+        yield return new WaitForSeconds(timeUIToDisappear);
+        _ui.Hide();
     }
 
     public void BlockMovement(Interactor interactor)
--- Assets/Scripts/Activities/Outside/FlowerUI.cs
+++ Assets/Scripts/Activities/Outside/FlowerUI.cs
@@ -6,17 +6,17 @@ public class FlowerUI : MonoBehaviour
 {
     [SerializeField] private CanvasGroup group;
     [SerializeField] private Image targetImage;
-    //[SerializeField] private FlowerImages flowerImages;
+    [SerializeField] private FlowerImages flowerImages;
 
     private Dictionary<FlowerState, Sprite> _state;
 
     private void Awake()
     {
-        //_state = new Dictionary<FlowerState, Sprite>(FlowerImages.entries.Length);
-        //foreach (var e in flowerImages.entries)
-        //    _state[e.flowerState] = e.sprite;
+        _state = new Dictionary<FlowerState, Sprite>(flowerImages.entries.Length);
+        foreach (var e in flowerImages.entries)
+            _state[e.flowerState] = e.sprite;
 
-        //Hide();
+        Hide();
     }
 
     public void Show()
