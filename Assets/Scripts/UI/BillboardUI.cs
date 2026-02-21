3f328a9389dbf6b516ca015042552cae2b31e131
ErNickIlSommo <nick.gianuzzi99@gmail.com>
2026-02-21 19:30:15 +0100

    Add flower activity in garden scene

--- Assets/Scripts/UI/BillboardUI.cs
+++ Assets/Scripts/UI/BillboardUI.cs
@@ -18,7 +18,7 @@ public class BillboardUI : MonoBehaviour
     {
         if (!targetCamera)
         {
-            Debug.Log("Ciao");
+            // Debug.Log("Ciao");
             return;
         }
 
